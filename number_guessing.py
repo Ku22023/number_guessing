a7738421fee1064d322297a0820df7177571e45e
Ku22023 <ku22023@my.westlake.school.nz>
2024-08-07 14:20:09 +1200

    boobie mo

--- number_guessing.py
+++ number_guessing.py
@@ -4,34 +4,39 @@ def welcome():
     print("\n----- Number Guessing Game ------\n")
     user_name = str(input("- What is your name: \n"))
     print(f"Welcome {user_name}!\nIn this game, you have three tries to guess a number 1-5.\nIf you get it wrong, the number changes\nYou need to guess five numbers correctly to win!")
-    thegame(rounds, wins)
-def thegame(rounds, wins):
+    thegame(wins, guesses, number)
+
+def generate_number():
+    number = random.randrange(1,5)
+    thegame(wins, guesses, number)
+
+def thegame(wins, guesses, number):
     game = True
-    guesses = 3
-    tries = 1
+    print(number)
     while game == True:
         if guesses >= 1:
-            user_guess = int(input(f"\n\nRound {rounds}\nWins: {wins}\nGuess a number between 1 and 5!\nYou have {guesses} guesse(s) left!\n")) 
-            number = 1 
-            number = random.randrange(1,5)
+            user_guess = int(input(f"\n\n\nWins: {wins}\nGuess a number between 1 and 5!\nYou have {guesses} guesse(s) left!\n")) 
             if user_guess == number:
-                print(f"Congratulations! You got it in {tries} tries!")
+                print(f"Congratulations! You got it!")
                 game = False
+                generate_number()
                 if wins != 4:
                     wins += 1
-                    thegame(rounds, wins)
+                    thegame(wins, guesses, number)
                 else:
                     print("You have beaten the game! Congrats!!")
+                    game = False
             else:
-                tries += 1
                 guesses -= 1
                 print(f"Wrong Number Please try again!")
-                thegame(rounds, wins)
-            rounds += 1
+                thegame(wins, guesses, number)
         else:
-            print("Game over! Please try again!!")
+            print("Round lost! New number generated!")
+            guesses = 3
             game = False
+            generate_number()
+            thegame(wins,guesses,number)
 
-rounds = 1
 wins = 0
+guesses = 3
 welcome()

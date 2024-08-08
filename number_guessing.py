fea1ae364cb2599812ae670a86c00a681b7dd7bc
Ku22023 <ku22023@my.westlake.school.nz>
2024-08-08 12:49:39 +1200

    skibidi mooooooooooooooooooooo

--- number_guessing.py
+++ number_guessing.py
@@ -4,11 +4,8 @@ def welcome():
     print("\n----- Number Guessing Game ------\n")
     user_name = str(input("- What is your name: \n"))
     print(f"Welcome {user_name}!\nIn this game, you have three tries to guess a number 1-5.\nIf you get it wrong, the number changes\nYou need to guess five numbers correctly to win!")
-    thegame(wins, guesses, number)
-
-def generate_number():
     number = random.randrange(1,5)
-    thegame(wins, guesses, number)
+    thegame(wins, guesses,number)
 
 def thegame(wins, guesses, number):
     game = True
@@ -18,14 +15,14 @@ def thegame(wins, guesses, number):
             user_guess = int(input(f"\n\n\nWins: {wins}\nGuess a number between 1 and 5!\nYou have {guesses} guesse(s) left!\n")) 
             if user_guess == number:
                 print(f"Congratulations! You got it!")
-                game = False
-                generate_number()
                 if wins != 4:
                     wins += 1
-                    thegame(wins, guesses, number)
+                    number = random.randrange(1,5)
+                    thegame(wins,guesses,number)
                 else:
                     print("You have beaten the game! Congrats!!")
                     game = False
+                    welcome()
             else:
                 guesses -= 1
                 print(f"Wrong Number Please try again!")
@@ -34,7 +31,7 @@ def thegame(wins, guesses, number):
             print("Round lost! New number generated!")
             guesses = 3
             game = False
-            generate_number()
+            random.randrange (1,5)
             thegame(wins,guesses,number)
 
 wins = 0

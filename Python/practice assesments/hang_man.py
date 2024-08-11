953e195e6e711057736b2b6ab9786cbb08fae001
oalnasra <oa22313@my.westlake.school.nz>
2024-08-12 10:15:11 +1200

    Got the random word working hope to get the game to continue after one geuss

---
+++ Python/practice assesments/hang_man.py
@@ -0,0 +1,22 @@
+import random
+
+def game():
+    #The words
+ words = ["jacky", "mo", "omar", "davidson", "brimely","tsai", "boba", "ryan", "roid", "kim"]
+
+
+
+
+def ran_word()
+#chooses random word
+ random_word = random.randint(0, len(words) - 1)
+
+#Selects random word
+ hangman_word = words[random_word]
+
+
+
+ print(hangman_word)
+
+ game()
+

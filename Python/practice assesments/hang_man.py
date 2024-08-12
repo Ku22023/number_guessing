da63117c0bbf11a5661b2c585c4c15ed0d5a5ae2
Ku22023 <ku22023@my.westlake.school.nz>
2024-08-13 09:50:12 +1200

    sabe ko naman

--- Python/practice assesments/hang_man.py
+++ Python/practice assesments/hang_man.py
@@ -1,22 +1,36 @@
 import random
+word_list = ["jacky", "mo", "omar", "davidson", "brimely","tsai", "boba", "ryan", "roid", "kim"]
 
-def game():
-    #The words
- words = ["jacky", "mo", "omar", "davidson", "brimely","tsai", "boba", "ryan", "roid", "kim"]
-
-
-
-
-def ran_word()
+def ran_word():
+    letters = []
 #chooses random word
- random_word = random.randint(0, len(words) - 1)
+    random_num = random.randint(0, len(word_list) - 1) #gets a random number from the list to grab a random item
+    hangman_word = word_list[random_num] #grabs random word
+    #breaks the word up into individual letters
+    return hangman_word
+    for i in letters: #redundant, delete later
+        print(i)
+    return letters #bring letters back to functions
 
-#Selects random word
- hangman_word = words[random_word]
+def game(word):
+    lives = 6
+    letters = list(word) 
+    print(letters) #shows the list of letters for the hangman
+    active = True
+    while active == True:
+        user_input = str(input("Type in a or word: "))
+        if len(user_input) >= 2:
+            print("word")
 
+        else:
+            print("letter")
+            if user_input in set(letters):
+                print("correct!!")
+                while user_input in set(letters):
+                    letters.remove(user_input)
+                print(letters)
+            else:
+                print("wrobg!!!")
 
-
- print(hangman_word)
-
- game()
-
+word = ran_word()
+game(word)
---
+++ Python/practice assesments/moman.py
@@ -0,0 +1,11 @@
+# List of words
+words = ["apple", "banana", "cherry", "date", "elderberry"]
+ 
+# Get a random index
+random_index = random.randint(0, len(words) - 1)
+ 
+# Select the word at the random index
+random_word = words[random_index]
+ 
+# Print the selected word
+print("Randomly selected word:", random_word)

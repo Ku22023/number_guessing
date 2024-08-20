520a7925e5e3db1b6337f49f810220c777758d11
Ku22023 <ku22023@my.westlake.school.nz>
2024-08-21 09:49:22 +1200

    boobs

--- number_guessing.py
+++ number_guessing.py
@@ -1,18 +1,28 @@
 import random
 
 def welcome():
+    """
+    This function starts the user and generates a random number
+    """
     print("\n----- Number Guessing Game ------\n")
     user_name = str(input("- What is your name: \n"))
-    print(f"Welcome {user_name}!\nIn this game, you have three tries to guess a number 1-5.\nIf you get it wrong, the number changes\nYou need to guess five numbers correctly to win!")
+    print(f"Welcome {user_name}!\nIn this game, you have three tries to\
+        guess a number 1-5.\nIf you get it wrong, the number changes\n\
+        You need to guess five numbers correctly to win!")
     number = random.randrange(1,5)
     thegame(wins, guesses,number)
 
 def thegame(wins, guesses, number):
+    """
+    The main code, checks if the number is right or wrong
+    """
     game = True
     print(number)
     while game == True:
         if guesses >= 1:            
-            user_guess = int(input(f"\n\n\nWins: {wins}\nGuess a number between 1 and 5!\nYou have {guesses} guesse(s) left!\n"))
+            user_guess = int(input(f"\n\n\nWins: {wins}\nGuess a \
+                number between 1 and 5!\nYou have {guesses} guesse(s)\
+                 left!\n"))
             try:
                 user_guess + 1
             except ValueError:
@@ -27,7 +37,8 @@ def thegame(wins, guesses, number):
                 else:
                     print("You have beaten the game! Congrats!!")
                     game = False
-                    user_guess = str(input("Type anything to play again. Type exit to exit!"))
+                    user_guess = str(input("Type anything to play \
+                        again. Type exit to exit!"))
                     if user_guess != "exit":
                         welcome()
                     else:
--- pizza_place.py
+++ pizza_place.py
@@ -18,10 +18,13 @@ def backst():
 def menu():
     #You should display a menu here! Make it look pretty!
     print("----------- MENU -----------\n")
-    print("[ #1. Mo's Special --- $16.30 ]\n - Random toppings; You get a surprise!\n")
+    print("[ #1. Mo's Special --- $16.30 ]\n - Random toppings; You get\
+     a surprise!\n")
     print("[ #2. Peperroni --- $12.50 ]\n - Classic Peperroni Pizza\n")
-    print("[ #3. Cheese --- $9.99 ]\n - Classic Pizza with no toppings\n")
-    print("[ #4. Skibidi Slicers --- $19.99 ]\n - a Pizza with all the toppings!\n")
+    print("[ #3. Cheese --- $9.99 ]\n - Classic Pizza with no\
+     toppings\n")
+    print("[ #4. Skibidi Slicers --- $19.99 ]\n - a Pizza with all the\
+     toppings!\n")
     print("[ #5. Meat Lovers --- $14.99 ]\n - Lots of meaty mo's\n")
     print("[ #6. Vegetarian --- $12.99 ]\n - Vegetarian Option")
     backst()
@@ -57,7 +60,9 @@ def vieworder():
 def orders():
     user_exit_a = False
     while user_exit_a != True:
-        print("\n----- Choose order Option: -----\n- 1. View order \n- 2. Add item(s) \n- 3. Remove item(s)\n- 4. Back to Main Menu")
+        print("\n----- Choose order Option: -----\n- 1. View order\
+             \n- 2. Add item(s) \n- 3. Remove item(s)\n- 4. Back to\
+              Main Menu")
         y = int(input("Select an option: "))
         if y == 1:
             vieworder()
@@ -68,10 +73,12 @@ def orders():
             while x != -1:
                 if x != -1:
                     if x >= 1 and x <= 6:
-                        x = int(input("Enter which Pizzas you would like to order. Type -1 to finish: "))
+                        x = int(input("Enter which Pizzas you would\
+                             like to order. Type -1 to finish: "))
                         order.append(x)
                     else:
-                        print("Error: Invalid Item. Please enter a number 1-6")
+                        print("Error: Invalid Item. Please enter a\
+                             number 1-6")
                         order.pop()
                         orders()
             order.pop()
@@ -82,7 +89,9 @@ def orders():
             x = 0
             while x != -1:
                 item = "error"
-                x = int(input("\nEnter the place of the item you want to remove, from the order list. Type -1 when you are finished: "))
+                x = int(input("\nEnter the place of the item you want\
+                     to remove, from the order list. Type -1 when you\
+                     are finished: "))
                 print(x)
                 if x != -1:
                     x -= 1
@@ -128,7 +137,8 @@ def total():
 def start():
     user_exit = False
     while user_exit != True:
-        print("\n--------Start-------\n- 1. Menu \n- 2. Order \n- 3. Calculate Total \n- 4. Exit\n--------------------")
+        print("\n--------Start-------\n- 1. Menu \n- 2. Order \n- 3.\
+             Calculate Total \n- 4. Exit\n--------------------")
         user_input = int(input("Choose what you want: \n"))
         if user_input == 1:
             menu()
